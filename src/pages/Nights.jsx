72e22d1a25a79bdf3bcb6db296703e967e7f0b01
arjit120604 <130384377+arjit120604@users.noreply.github.com>
2024-02-08 22:29:41 +0530

    coming soon

--- src/pages/Nights.jsx
+++ src/pages/Nights.jsx
@@ -1,26 +1,42 @@
 import nights from "../assets/images/nights.png";
 import TopBar from "../components/TopBar";
 import night_logo from "../assets/images/nights_logo.png";
+import coming_soon from "../assets/images/coming_soon.png";
 
 function Nights() {
-    return (
-        <>
-            <div
-                className="h-full min-h-screen relative bg-cover flex flex-col items-center"
-                style={{
-                    backgroundImage: `url(${nights})`,
-                    backgroundRepeat: "no-repeat",
-                    marginLeft: 'auto',
-                    marginRight: 'auto',
-                }}
-            >
-                <TopBar />
-                <div className="flex-grow flex items-center  flex-col justify-center">
-                    <img src={night_logo} alt="Nights logo" />
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div
+        className="h-full min-h-screen relative bg-cover flex flex-col items-center"
+        style={{
+          backgroundImage: `url(${nights})`,
+          backgroundRepeat: "no-repeat",
+          marginLeft: "auto",
+          marginRight: "auto",
+        }}
+      >
+        <TopBar />
+        <div className="flex-grow flex items-center  flex-col justify-center">
+          <img src={night_logo} alt="Nights logo" />
+        </div>
+      </div>
+      <div
+        style={{
+          backgroundImage: `url(${coming_soon})`,
+          backgroundRepeat: "no-repeat",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          minHeight: "100vh",
+          backgroundSize: "cover",
+        }}
+      >
+        <div className="lg:text-6xl text-4xl z-10 font-azonix text-white">
+          COMING SOON
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Nights;
